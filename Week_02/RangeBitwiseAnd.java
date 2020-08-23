9971e0c3dafaf53c63492a3c9c82ae0d33a9106a
loloyazolo <loloyazolo@qq.com>
2020-08-23 22:38:41 +0800

    按位与问题

--- Week_02/RangeBitwiseAnd.java
+++ Week_02/RangeBitwiseAnd.java
@@ -2,12 +2,29 @@ package week2;
 
 public class RangeBitwiseAnd {
     public static void main(String[] args) {
-
+        int m = 1, n = 7;
+        RangeBitwiseAnd rangeBitwiseAnd = new RangeBitwiseAnd();
+        System.out.println(rangeBitwiseAnd.rangeBitwiseAnd(m, n));
     }
 
+    //两个数同时右移
     public int rangeBitwiseAnd(int m, int n) {
-        return 0;
+        int shift = 0;
+        //不停将两个数右移，直到两数相等，等到两数的公共前缀
+        while (m < n) {
+            m >>= 1;
+            n >>= 1;
+            shift++;
+        }
+        return m << shift;
     }
 
-    
+    //n & (n - 1)会把最右边的1都置为0
+    public int rangeBitwiseAnd2(int m, int n) {
+        while (m < n) {
+            n = n & (n - 1);
+        }
+        return n;
+    }
+
 }
